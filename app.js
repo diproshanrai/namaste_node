a9af7e9ddf84e1d41587ab51dce0737c2baa6287
diproshanrai <diproshanrai@gmail.com>
2024-08-27 07:51:21 +0545

    cjs esm done

---
+++ app.js
@@ -0,0 +1,12 @@
+// require("./abc")
+// const calculateSum = require("./sum")
+
+import { calculateSum } from "./sum";
+
+ var a = 10;
+ var b = 102;
+
+// var c = a + b 
+
+
+calculateSum(a,b);
---
+++ sum.js
@@ -0,0 +1,12 @@
+var x = "i am here"
+
+export function calculateSum(a,b){
+   var c= a+b 
+    console.log(c);
+}
+
+console.log(x);
+
+
+
+// module.exports = calculateSum;
